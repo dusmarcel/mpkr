ce43f7a5353ff0cdccdf461f59a6df507ad9783c
dusmarcel <marcel@keienb.org>
2025-07-08 23:17:10 +0200

    MPKR 25 -> MPKR

--- src/components/intro.rs
+++ src/components/intro.rs
@@ -5,7 +5,7 @@ pub fn Intro() -> impl IntoView {
     view! {
         <div class="container max-w-screen-xl mx-auto px-4 bg-linear-to-b from-stone-50 to-stone-300">
             <h1 class="pt-4 text-3xl font-medium">
-                "Migrationsrechtlicher Prozesskostenrechner (Stand 2025)"
+                "Migrationsrechtlicher Prozesskostenrechner"
             </h1>
             <p>
                 "Erstellt von "<a class="text-blue-600 hover:underline hover:text-violet-600" href="https://aufentha.lt">"Marcel Keienborg"</a>". Bitte beachte unbedingt auch die
